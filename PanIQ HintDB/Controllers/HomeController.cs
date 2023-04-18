f3597d46c86864fdd8e67148c9ffaa612caf067c
Lorenzo <lorenzo.joseph.dominguez@gmail.com>
2023-04-17 19:25:16 -0700

    Add project files.

---
+++ PanIQ HintDB/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PanIQ_HintDB.Models;
+using System.Diagnostics;
+
+namespace PanIQ_HintDB.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ PanIQ HintDB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PanIQ_HintDB.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
